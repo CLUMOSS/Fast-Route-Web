a7c5fd50bb5bcb7e863a4051a3516ba33195310e
Akshay Sharma <akshayne912@gmail.com>
2023-11-17 19:19:09 +0530

    created the sign up page

--- fastrouteweb/src/App.js
+++ fastrouteweb/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import SignUp from './pages/signup/signup';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      
         <a>
         <SignUp></SignUp>
         </a>
