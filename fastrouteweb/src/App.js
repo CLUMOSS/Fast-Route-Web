723fc8f85a624d881a8cc391349f60652508c590
Nikita <nikitanb04@gmail.com>
2023-11-17 18:58:47 +0530

    Made the Navbar

--- fastrouteweb/src/App.js
+++ fastrouteweb/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+
+//import './App.css';
 import SignUp from './pages/signup/signup';
+import Navbar from './components/common/Navbar/Navbar';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a>
+       
         <SignUp></SignUp>
-        </a>
+        
       </header>
     </div>
   );
---
+++ fastrouteweb/src/components/common/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../../../styles/Navbar.css'
+
+
+const Navbar = () => {
+    return (
+        <header className="navbar d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-2 mb-4 px-5">
+      <div className="col-md-3 mb-2 mb-md-0">
+        <img src = "logo2 2.png" alt = "fastRoute logo"></img>
+      </div>
+
+      <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="#" className="nav-item px-3">Home</a></li>
+        <li><a href="#" className="nav-item px-3">About Us</a></li>
+        <li><a href="#" className="nav-item px-3">Careers</a></li>
+        <li><a href="#" className="nav-item px-3">Why Choose Us</a></li>
+        <li><a href="#" className="nav-item px-3">Contact Us</a></li>
+      </ul>
+
+      <div className="col-md-3 text-end">
+        <button className="login">Login</button>
+      </div>
+    </header>
+    );
+}
+
+export default Navbar;
