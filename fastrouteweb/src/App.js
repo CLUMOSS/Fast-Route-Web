67e6feb50c821dd08c101f61db57c39996614976
Aditya Singh <103613774+AdityaSingh2005@users.noreply.github.com>
2023-11-19 15:30:14 +0530

    Committing Signing Page

--- fastrouteweb/src/App.js
+++ fastrouteweb/src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import SignUp from './pages/signup/signup';
 import Navbar from './components/common/Navbar/Navbar';
+import SignIn from './pages/signin/signin';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <a>
-        <SignUp></SignUp>
+        <SignIn></SignIn>
         </a>
       </header>
     </div>
