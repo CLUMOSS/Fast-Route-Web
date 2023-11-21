471fe9f7d9d51a6ae77bb7dc6c5fa46e6bff66e4
Akshay Sharma <akshayne912@gmail.com>
2023-11-21 23:04:43 +0530

    about us page

---
+++ fastrouteweb/src/pages/signup/signuppage.js
@@ -0,0 +1,16 @@
+import Navbar from "../../components/common/Navbar/Navbar";
+import SignUp from "./signup";
+const Homepage = () => {
+    return (
+              <div className="">
+                <header className="App-header">
+                  <a>
+                  <SignUp></SignUp>
+                  </a>
+          
+                </header>
+              </div>
+            );
+          }
+export default Homepage;
+          
