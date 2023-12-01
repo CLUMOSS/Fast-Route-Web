e469ce5c379c9a160dedc458f44fcf79358b4b57
Karan Bhatt <80518096+karanbhatt230803@users.noreply.github.com>
2023-12-01 19:44:09 +0530

    Update App.js

--- fastrouteweb/src/App.js
+++ fastrouteweb/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 // import Navbar from './components/common/Navbar/Navbar';
  //import ContactUs from './pages/ContactUs';
 // import Footer from './components/common/Footer/footer';
-//import Homepage from "./pages/home/homepage";  // Keep this line if you want to add the Homepage component
+import Homepage from "./pages/home/homepage";  // Keep this line if you want to add the Homepage component
 //  import Vehical from  "./pages/vehicle-insurance/vehicle";
 // import Career from './pages/careers/career';
 // import AboutUs from './pages/about/aboutpage';
